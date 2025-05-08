1550177526469a245d57d675db9f165db5b052e5
Carlos E <github@capitan.simplelogin.com>
2025-05-08 13:59:54 -0500

    Made recursion project work

---
+++ fibonacci.rb
@@ -0,0 +1,27 @@
+def fibs(number)
+  fibonacci_array = []
+  length = number - 2
+  first = 0
+  second = 1
+  fibonacci_array.push(first, second)
+  length.times {|i|
+      result = first + second
+      first = second
+      second = result 
+      fibonacci_array << result
+  }
+  puts fibonacci_array
+end
+
+def fibs_rec(result = [0,1], number)
+  fibonacci_array = []
+  if result.length >= number
+    return result
+  else
+    next_value = result[-1] + result[-2]
+    result << next_value
+    fibs_rec(result, number)
+  end
+end
+
+puts fibs_rec(8)
